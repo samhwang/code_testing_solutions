0f6947e0b6e1daf57acbc0854b78f4f3274091f5
Sam Huynh <samhwang2112.dev@gmail.com>
2021-03-28 01:53:59 +0000

    Add birthday cake candles challenge

---
+++ hackerrank/birthdayCakeCandles.js
@@ -0,0 +1,12 @@
+/**
+ * Count the number of the tallest candles on a birthday cake
+ * @param {number[]} candles 
+ * @returns {number}
+ */
+function birthdayCakeCandles(candles) {
+    const numCandles = candles.length;
+    const sortedCandles = candles.sort((prev, next) => prev - next);
+    return sortedCandles
+        .filter((candle) => candle === sortedCandles[numCandles - 1])
+        .length;
+}
