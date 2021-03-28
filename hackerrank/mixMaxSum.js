735a86e30c2b82bf5d51243093a73d7a0b183dc3
Sam Huynh <samhwang2112.dev@gmail.com>
2021-03-28 01:32:37 +0000

    Add minmaxsum challenge

---
+++ hackerrank/mixMaxSum.js
@@ -0,0 +1,14 @@
+/**
+ * Return the min and max possible of a sum of 4 numbers in a num array
+ * @param {number[]} arr
+ */
+function miniMaxSum(arr) {
+    const sortedArray = arr.sort();
+    function getSumOfElements(start, end = undefined) {
+        return sortedArray.slice(start, end).reduce((total, num) => total + num, 0);
+    }
+    const minSum = getSumOfElements(0, 4);
+    
+    const maxSum = getSumOfElements(1);
+    console.log(`${minSum} ${maxSum}`);
+}
